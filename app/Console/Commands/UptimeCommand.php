9b32c56b4003d32198559de5ee3d7d9dd9e3ef48
Fikret CURE <fikretcure@gmail.com>
2025-08-21 14:14:47 +0300

    first commit

---
+++ app/Console/Commands/UptimeCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class UptimeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:uptime-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $url = 'https://dentalasistanim.com';
+
+        try {
+            $response = Http::get($url);
+            $this->info("Request sent to {$url}. Status: " . $response->status());
+
+            info("Request sent to {$url}. Status: " . $response->status());
+
+        } catch (\Exception $e) {
+            $this->error("Request failed: " . $e->getMessage());
+        }
+    }
+}
